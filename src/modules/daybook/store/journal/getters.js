ab141153f122567d5c1412aba6160e5ac46139aa
Jadmir <jadmir1111@gmail.com>
2021-09-19 22:15:10 -0500

    segundo

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -3,8 +3,11 @@
     //return state
 //}
 
-export const getEntriesByTern =  (/*state*/) => {
-    
+export const getEntriesByTerm = ( state ) => ( term = '' ) => {
+
+    if ( term.length === 0 ) return state.entries
+
+    return state.entries.filter( entry => entry.text.toLowerCase().includes( term.toLocaleLowerCase() ) )
 }
 
 export const getEntriesByid =  (/*state*/) => {
